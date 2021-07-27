4034de20b07c5c16d01dee81ca52aa6b778caa72
Jefferson Santos <jeffersonscr12@gmail.com>
2021-07-27 19:00:30 -0300

    home

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,20 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    try {
-      const novoAluno = await Aluno.create({
-        nome: 'jefferson',
-        sobrenome: 'santos',
-        email: 'jeff@gamil.com',
-        idade: 112,
-        peso: 300,
-        altura: 2.5,
-      });
-      res.json(novoAluno);
-    } catch (error) {
-      console.log(error);
-    }
+    res.json('Index');
   }
 }
 
